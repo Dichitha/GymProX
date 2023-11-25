9b6aeb7e3c3de233b3bb93cb918c64a98e517c5b
Dichitha <92388117+Dichitha@users.noreply.github.com>
2023-11-25 15:29:10 +0530

    Add files via upload

---
+++ customer/includes/sidebar.php
@@ -0,0 +1,7 @@
+<div id="sidebar" style="background-color:#080145; color:white"><a href="#" class="visible-phone"><i class="icon icon-home" ></i> Dashboard</a>
+  <ul style="background-color:#080145">
+    <li class="<?php if($page=='dashboard'){ echo 'active'; }?>" style="background-color:#080145; color:white"><a href="index.php"><i class="icon icon-home"></i> <span>Dashboard</span></a> </li>
+    <li class="<?php if($page=='todo'){ echo 'active'; }?>" style="background-color:#080145; color:white"> <a href="to-do.php"><i class="icon icon-pencil"></i> <span>To-Do</span></a>
+    <li class="<?php if($page=='report'){ echo 'active'; }?>" style="background-color:#080145; color:white"><a href="my-report.php"><i class="icon icon-file"></i> <span>Reports</span></a></li>
+  </ul>
+</div>
---
+++ customer/includes/topheader.php
@@ -0,0 +1,15 @@
+<div id="user-nav" class="navbar navbar-inverse" style="background-color:#080145; color:white">
+  <ul class="nav right" style="background-color:#080145">
+    <li  class="dropdown" id="profile-messages" style="background-color:#080145; color:white"><a title="" href="#" data-toggle="dropdown" data-target="#profile-messages" class="dropdown-toggle"><i class="icon icon-user"></i>  <span class="text">Welcome Customer</span><b class="caret"></b></a>
+      <ul class="dropdown-menu" style="background-color:#080145">
+        <li style="background-color:#080145"><a href="../pages/my-report.php"><i class="icon-user"></i> My Report</a></li>
+        <li class="divider" style="background-color:#080145"></li>
+        <li style="background-color:#080145"><a href="to-do.php"><i class="icon-check"></i> My Tasks</a></li>
+        <li class="divider " style="background-color:#080145"></li>
+        <li style="background-color:#080145"><a href="../logout.php" ><i class="icon-key"></i> Log Out</a></li>
+      </ul>
+    </li>
+    
+    <li class="" style="background-color:#080145"><a title="" href="../logout.php"><i class="icon icon-share-alt"></i> <span class="text">Logout</span></a></li>
+  </ul>
+</div>
